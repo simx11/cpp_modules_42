ea9065d69fa83c7c22fb04b9f1aa3b842f53211c
Simon Hoffmann <shoffman@c2r1p7.42vienna.com>
2023-03-02 13:31:10 +0100

    fix

--- cpp_module_01/ex04/main.cpp
+++ cpp_module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: shoffman <shoffman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 14:27:44 by shoffman          #+#    #+#             */
-/*   Updated: 2023/02/21 12:00:01 by shoffman         ###   ########.fr       */
+/*   Updated: 2023/03/02 13:30:54 by shoffman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,10 @@ void replace(std::string& line, std::string search, std::string replace)
 
 int	main(int argc, char *argv[])
 {
-	std::string in_file = argv[1];
-	std::string out_file = in_file + ".replace";
 	if(argc != 4)
 		return(std::cout << "Invalid amount of arguments.\n", 1);
+	std::string in_file = argv[1];
+	std::string out_file = in_file + ".replace";
 	std::ifstream input(argv[1]);
 	if(!input.is_open())
 		return(std::cout << "File could not be opened\n", 1);
